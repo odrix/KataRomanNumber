44d28412f20040ba9a4ea0233e5b54210b61a4c0
odrix <adrilesage@gmail.com>
2022-01-30 11:17:57 +0100

    up to 39

--- romanNumberConverter.js
+++ romanNumberConverter.js
@@ -1,13 +1,26 @@
+const numbers = [
+    { decimal: 10, roman: 'X' },
+    { decimal: 5, roman: 'V' }
+]
+
 exports.converter = function converter(decimalNumber) {
 
     let result = '';
-    if (decimalNumber >= 5 - 1) {
-        result = 'V';
-        decimalNumber -= 5;
+
+    while (decimalNumber > 3) {
+        numbers.forEach(element => {
+            if (decimalNumber >= element.decimal - 1) {
+                result += element.roman;
+                decimalNumber -= element.decimal;
+            }
+        });
     }
 
     if (decimalNumber === -1)
-        return 'I' + result;
+        if (result.length > 1)
+            return result.substring(0, result.length - 1) + 'I' + result.substring(result.length - 1);
+        else
+            return 'I' + result;
 
     result += 'I'.repeat(decimalNumber);
 
--- test/test.js
+++ test/test.js
@@ -7,4 +7,42 @@ describe('Roman number converter', function() {
         let result = romanConverter.converter(1);
         assert.equal(result, 'I');
     });
+
+    it('2 should return II', function() {
+        let result = romanConverter.converter(2);
+        assert.equal(result, 'II');
+    });
+
+    it('5 should return V', function() {
+        let result = romanConverter.converter(5);
+        assert.equal(result, 'V');
+    });
+
+    it('4 should return IV', function() {
+        let result = romanConverter.converter(4);
+        assert.equal(result, 'IV');
+    });
+
+    it('6 should return VI', function() {
+        let result = romanConverter.converter(6);
+        assert.equal(result, 'VI');
+    });
+
+
+    it('10 should return X', function() {
+        let result = romanConverter.converter(10);
+        assert.equal(result, 'X');
+    });
+
+    it('9 should return IX', function() {
+        let result = romanConverter.converter(9);
+        assert.equal(result, 'IX');
+    });
+
+    it('14 should return XIV', function() {
+        let result = romanConverter.converter(14);
+        assert.equal(result, 'XIV');
+    });
+
+
 });
